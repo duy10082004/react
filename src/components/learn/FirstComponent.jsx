0a5ace97ea599b9c7f0ac445aac180082db8cd99
Phan Nhat Duy <abcduy39@gmail.com>
2024-10-13 10:55:21 +0700

    #30

--- src/components/learn/FirstComponent.jsx
+++ src/components/learn/FirstComponent.jsx
@@ -1,7 +1,11 @@
+import './style.css';
 
 const MyComponent = () => {
     return (
-      <div>hehehehe</div>
+        <>
+            <div>hehehehe</div>
+            <div className='child'>hahahahha</div>
+        </>
     );
   }
 
